05a63eb09925d1014320af338bf846d5794db5fe
Gudetomi <gustavodetomi@gmail.com>
2019-11-20 11:00:34 -0300

    arquivos

--- main.c
+++ main.c
@@ -2,3 +2,21 @@
 #include<stdlib.h>
 #include<string.h>
 
+int main(int argc,char *argv[]){
+
+    /*trata arquivo de entrada, após tratado, verificar qual tipo de solução desejada:
+    PD = Programação Dinamica, AG = Algoritmo Guloso, FB = Força Bruta, e sem seguida chamar
+    a respectiva função passando como parametro os argumentos de entrada e o vetor das distancias*/
+
+
+    if(argv[0] == 'PD'){
+        
+    }
+    else if(argv[0] == 'AG'){
+
+    }
+    else if(argv[0]== 'FB'){
+
+    }
+
+}
