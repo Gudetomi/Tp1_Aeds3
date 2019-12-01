042afc095553ecf631324d27bd9277b36f907acc
Rodrigo Ramos de Araujo <36712976+RodrigoRArauj@users.noreply.github.com>
2019-12-01 00:37:32 -0300

    Update funcoes.h
    inseridas funções q auciliam o força bruta

--- funcoes.h
+++ funcoes.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 typedef struct star_death{
     int **matriz_d, k_saltos, n_planetas, final_plan;
@@ -13,7 +14,7 @@ void executa_pd(int nave,Stardeath *star_death3);
 void executa_ag(int nave,Stardeath *star_death3);
 
 //Executa Força Bruta
-void executa_fb(int nave,Stardeath *star_death3);
+void executa_fb(int nave, Stardeath *star_death2);
 
 //Interpretador das Entradas
 Stardeath *imperial_plan(int *nave);
@@ -23,3 +24,12 @@ void libera_matriz(Stardeath *star_death3);
 
 //Gera Saida
 void gera_saida();
+
+//FB - Compara maior a caminho final
+void compara(int*aux,int r,int *maior,int *s);
+
+//espelha matriz_d pela diagonal principal
+void mirror_matriz(int g, Stardeath *star_death2);
+
+//calcula as possiveis combinacoes de forma recursiva
+void combinationUtil(int **arr, int *aux, int inic, int fim, int index,int c, int r,int k,int *s);
