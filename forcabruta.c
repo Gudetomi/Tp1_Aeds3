6a2d327e3352764acd1dd26d6ea75cf5bdc6efcb
Rodrigo Ramos de Araujo <36712976+RodrigoRArauj@users.noreply.github.com>
2019-12-02 13:08:57 -0300

    Update forcabruta.c

--- forcabruta.c
+++ forcabruta.c
@@ -1,12 +1,12 @@
 #include "funcoes.h"
 
 void executa_fb(int nave, Stardeath *star_death2){
-	Stardeath *star_death3 = star_death2;
-	int *aux;
-	aux=(int*)malloc(sizeof(int));
-	for(int k = 0; k < nave;k++){
-		mirror_matriz(k,star_death3);
-		(int*) realloc (aux, star_death3[k].k_saltos+1* sizeof(int));
-		combinationUtil(star_death3[k].matriz_d, aux, 1, star_death3[k].n_planetas+1, 0, 0, star_death3[k].k_saltos+1,0,&star_death3[k].final_plan);
+    Stardeath *star_death3 = star_death2;
+    for(int i = 0; i < nave; i++){
+        int *aux;
+        mirror_matriz(i,star_death3);
+        aux = (int*) calloc ( star_death3[i].k_saltos+1 , sizeof(int));
+        combinationUtil(star_death3[i].matriz_d,aux, 1, star_death3[i].n_planetas+1, 0, 0, star_death3[i].k_saltos+1,0,&star_death3[i].final_plan);
+        free(aux);
 	}
 }
